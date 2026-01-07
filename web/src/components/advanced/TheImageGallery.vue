02301000724061b319e1214c4450e8e7bded6c15
L1nSn0w <l1nsn0w@qq.com>
2026-01-07 22:47:16 +0800

    fix: Adjust hover scale effect on images in TheImageGallery component from 1.02 to 1.01 for a more subtle interaction.

--- web/src/components/advanced/TheImageGallery.vue
+++ web/src/components/advanced/TheImageGallery.vue
@@ -215,7 +215,7 @@ onBeforeUnmount(() => {})
 }
 
 button:hover .echoimg {
-  transform: scale(1.02);
+  transform: scale(1.01);
   box-shadow:
     0 2px 4px rgba(0, 0, 0, 0.04),
     0 4px 8px rgba(0, 0, 0, 0.04),
